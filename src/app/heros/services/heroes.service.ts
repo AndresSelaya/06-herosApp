09dcf326c68aaed9a1687c88bb418fcc9dd7a433
Omar Selaya <201900837@est.umss.edu>
2026-02-11 23:13:13 -0400

    crud hero service

--- src/app/heros/services/heroes.service.ts
+++ src/app/heros/services/heroes.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Hero } from "../interfaces/hero.interface";
 import { environments } from "../../../environments/environments";
-import { catchError, Observable, of } from "rxjs";
+import { catchError, map, Observable, of } from "rxjs";
 
 
 @Injectable({providedIn: 'root'})
@@ -25,5 +25,23 @@ export class HeroesService {
 
   getSuggestions(query: string): Observable<Hero[]> {
     return this.http.get<Hero[]>(`${this.baseUrl}/heroes?q=${ query }&_limit=6`);
+  }
+
+  addHero( hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(`${this.baseUrl}/heroes`, hero);
+  }
+
+  updateHero( hero: Hero): Observable<Hero> {
+    if(!hero.id) throw new Error('Hero id is required');
+
+    return this.http.patch<Hero>(`${this.baseUrl}/heroes/${hero.id}`, hero);
+  }
+
+  deleteHerobyId(id: string): Observable<boolean> {
+    return this.http.delete(`${this.baseUrl}/heroes/${id}`)
+    .pipe(
+      catchError(err => of(false)),
+      map(resp => true)
+    )
   }
 }
